82a66db0a807ab2376bdb78108d867f928b048dc
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-02 19:37:09 -0300

    first commit - created project

---
+++ src/test/java/com/do55anto5/workshopmongo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.do55anto5.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
