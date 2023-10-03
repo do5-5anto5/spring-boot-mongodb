77a05db455471186773242b30a78edab2ac4b6c2
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-03 18:48:28 -0300

    user insert

--- src/main/java/com/do55anto5/workshopmongo/services/UserService.java
+++ src/main/java/com/do55anto5/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.do55anto5.workshopmongo.domain.User;
+import com.do55anto5.workshopmongo.dto.UserDTO;
 import com.do55anto5.workshopmongo.repository.UserRepository;
 import com.do55anto5.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
