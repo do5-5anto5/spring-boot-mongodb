31c358afd75aba1aeea205111af8da74197aa2fa
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-10-04 21:38:38 -0300

    simple search with @Query ; readme update

--- src/main/java/com/do55anto5/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/do55anto5/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,18 @@
+//see https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+
 package com.do55anto5.workshopmongo.repository;
 
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.do55anto5.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
